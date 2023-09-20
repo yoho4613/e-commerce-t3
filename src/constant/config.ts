076a8f20c7a6dbaee8a8c9729d79bd817969d12c
박지호 <yoho4613@gmail.com>
2023-09-20 18:35:56 +1200

    update operator

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+export const BASE_URL = process.env.BASE_URL ?? "http://localhost:3000";
 
 export const defaultUserDetail = {
   id: "",

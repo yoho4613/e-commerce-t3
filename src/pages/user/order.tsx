e19d9c258a3eb2c4f73d195e9da1c78754dc3eda
박지호 <yoho4613@gmail.com>
2023-10-11 19:13:26 +1300

    user review

--- src/pages/user/order.tsx
+++ src/pages/user/order.tsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { toast } from "react-hot-toast";
+import { BsStar, BsStarFill } from "react-icons/bs";
 import Spinner from "~/components/global/Spinner";
 import { useStateContext } from "~/context/userDetailContext";
 import { api } from "~/utils/api";
-
-
 
 const Order: FC = ({}) => {
   const { userDetail } = useStateContext();
   const { data: orders, isError } = api.order.getUserOrder.useQuery({
     userId: userDetail.id,
   });
+  const { mutate: createReview } = api.review.createReview.useMutation({
+    onSuccess: (res) => {
+      toast.success("review successfully submitted");
+      refetch().then(res => res).catch(err => console.log(err))
+      setReview("");
+    },
+  });
+  const { data: userReviews, refetch } = api.review.getUserReviews.useQuery({
+    userId: userDetail.id,
+  });
+  console.log(userReviews?.map((review) => review.productId));
+  const [review, setReview] = useState<string>("");
 
   if (isError) {
     return (
@@ -20,9 +32,75 @@ const Order: FC = ({}) => {
     );
   }
 
+  const ReviewForm = () => {
+    const [form, setForm] = useState({ comment: "", star: 0 });
+
+    return (
+      <div className="fixed left-1/2 mx-auto flex max-w-xl -translate-x-1/2 flex-col gap-2 overflow-hidden border-2 bg-gray-300 p-4 dark:bg-buttonBlack">
+        <h2 className="text-2xl font-bold">Review Form</h2>
+
+        <div className="flex flex-col items-center gap-2">
+          <label>Comment</label>
+          <textarea
+            name="description"
+            rows={8}
+            className="h-12 w-full grow rounded-sm border-none bg-gray-200 p-1"
+            placeholder="comment..."
+            onChange={(e) =>
+              setForm((prev) => ({ ...prev, comment: e.target.value }))
+            }
+            value={form.comment}
+          />
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <label>Star</label>
+          <div className="flex gap-2">
+            {[1,2,3,4,5].map((_, i) =>
+              form.star > i ? (
+                <BsStarFill
+                  key={i}
+                  size={20}
+                  className="text-[#ffc107] sm:mr-2"
+                  onClick={(e) => setForm((prev) => ({ ...prev, star: i + 1 }))}
+                />
+              ) : (
+                <BsStar
+                  key={i}
+                  size={20}
+                  className="text-[#ffc107] sm:mr-2"
+                  onClick={(e) => setForm((prev) => ({ ...prev, star: i + 1 }))}
+                />
+              ),
+            )}
+          </div>
+        </div>
+
+        {/* {error && <p className="text-xs text-red-600">{error}</p>} */}
+        <button
+          className="h-12 rounded-sm bg-gray-200 disabled:cursor-not-allowed"
+          disabled={form.star < 1 || !form.comment || !review}
+          onClick={() => {
+            createReview({
+              userId: userDetail.id,
+              productId: review,
+              star: form.star,
+              comment: form.comment,
+            });
+          }}
+        >
+          Submit
+        </button>
+        <button className="btn--red px-4 py-2" onClick={() => setReview("")}>
+          Cancel
+        </button>
+      </div>
+    );
+  };
+
   return (
     <div className="my-4 max-w-[1280px] sm:my-12">
       <h1 className="text-center text-xl font-bold sm:text-2xl">My Order</h1>
+      {review && <ReviewForm />}
       <div className="space-y-2 px-4">
         {orders ? (
           orders.map((order) => (
@@ -60,7 +138,19 @@ const Order: FC = ({}) => {
                     />
                     <p>Quantity: {product.quantity}</p>
                     <p>${Number(product.price) * product.quantity}</p>
-                    <button  className="px-0.5 py-2 border-2 rounded-sm hover:bg-gray-400">Review Product</button>
+                    {userReviews &&
+                    userReviews
+                      .map((review) => review.productId)
+                      .includes(product.id) ? (
+                      <p className="text-gray-600">Review Submitted</p>
+                    ) : (
+                      <button
+                        onClick={() => setReview(product.id)}
+                        className="rounded-sm border-2 px-0.5 py-2 hover:bg-gray-400"
+                      >
+                        Review Product
+                      </button>
+                    )}
                   </div>
                 ))}
               </div>
--- src/server/api/routers/review.ts
+++ src/server/api/routers/review.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, publicProcedure, userProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 
 export const reviewRouter = createTRPCRouter({
@@ -7,7 +7,6 @@ export const reviewRouter = createTRPCRouter({
     .input(z.object({ id: z.string() }))
     .query(async ({ ctx, input }) => {
       const { id } = input;
-
 
       const reviews = await ctx.prisma.review.findMany({
         where: {
@@ -20,4 +19,50 @@ export const reviewRouter = createTRPCRouter({
 
       return reviews ? reviews : [];
     }),
+  createReview: userProcedure
+    .input(
+      z.object({
+        productId: z.string(),
+        userId: z.string(),
+        star: z.number(),
+        comment: z.string(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { userId, productId, comment, star } = input;
+      const review = await ctx.prisma.review.create({
+        data: {
+          userId,
+          productId,
+          comment,
+          star,
+        },
+      });
+
+      return review;
+    }),
+  findReview: userProcedure
+    .input(z.object({ userId: z.string(), productId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId, productId } = input;
+
+      const review = await ctx.prisma.review.findFirst({
+        where: {
+          userId,
+          productId,
+        },
+      });
+
+      return review ? review : false;
+    }),
+  getUserReviews: userProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      }),
+    )
+    .query(
+      async ({ ctx, input }) =>
+        await ctx.prisma.review.findMany({ where: { userId: input.userId } }),
+    ),
 });
